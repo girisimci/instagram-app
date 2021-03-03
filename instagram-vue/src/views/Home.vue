fc5980392480231a35d96085a49c41fe21144a8f
Mehmet Sait Işık <mehmet.isik@morhipo.com>
2021-03-03 15:16:08 +0300

    svg dosyaları eklendi

--- instagram-vue/src/views/Home.vue
+++ instagram-vue/src/views/Home.vue
@@ -5,17 +5,20 @@
    <CustomText tag="p" size="small">Selam</CustomText>
    <CustomText tag="p" size="xsmall">Selam</CustomText>
    <CustomText tag="p" size="xxsmall">Selam</CustomText>
+   <IconHome/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import CustomText from '@/components/CustomText'
+import CustomText from '@/components/CustomText';
+import IconHome from '../icons/home.svg';
 
 export default {
   name: "Home",
   components:{
-    CustomText
+    CustomText,
+    IconHome
   }
 };
 </script>
